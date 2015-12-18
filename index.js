a6dada677d008465ea667d2ba7a824788c7918f9
calidion <calidion@gmail.com>
2015-12-18 21:24:45 +0800

    enabled auth & settings

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
 var request = require("node-weixin-request");
 var util = require("node-weixin-util");
+var auth = require('node-weixin-auth');
+var settings = require('node-weixin-settings');
+
 var baseUrl = 'https://api.weixin.qq.com/cgi-bin/qrcode/';
 
 module.exports = {
   qrcode: {
     temporary: {
-      create: function (app, auth, id, cb) {
+      create: function (app, id, cb) {
         auth.determine(app, function () {
+          var authData = settings.get(app.id, 'auth');
           var data = {
             expire_seconds: 604800,
             action_name: "QR_SCENE",
@@ -18,15 +22,16 @@ module.exports = {
             }
           };
           var url = baseUrl + 'create' + '?' + util.toParam({
-              access_token: app.auth.accessToken
+              access_token: authData.accessToken
             });
           request.json(url, data, cb);
         });
       }
     },
     permanent: {
-      create: function (app, auth, id, cb) {
+      create: function (app, id, cb) {
         auth.determine(app, function () {
+          var authData = settings.get(app.id, 'auth');
           var data = {
             action_name: "QR_LIMIT_SCENE",
               action_info: {
@@ -36,13 +41,14 @@ module.exports = {
             }
           };
           var url = baseUrl + 'create' + '?' + util.toParam({
-              access_token: app.auth.accessToken
+              access_token: authData.accessToken
             });
           request.json(url, data, cb);
         });
       },
-      createString: function (app, auth, string, cb) {
+      createString: function (app, string, cb) {
         auth.determine(app, function () {
+          var authData = settings.get(app.id, 'auth');
           var data = {
             action_name: "QR_LIMIT_STR_SCENE",
             action_info: {
@@ -52,7 +58,7 @@ module.exports = {
             }
           };
           var url = baseUrl + 'create' + '?' + util.toParam({
-              access_token: app.auth.accessToken
+              access_token: authData.accessToken
             });
           request.json(url, data, cb);
         });
@@ -60,14 +66,15 @@ module.exports = {
     }
   },
   url: {
-    shorten: function (app, auth, longUrl, cb) {
+    shorten: function (app, longUrl, cb) {
       auth.determine(app, function () {
+        var authData = settings.get(app.id, 'auth');
         var data = {
           action: 'long2short',
           long_url: longUrl
         };
         var url = 'https://api.weixin.qq.com/cgi-bin/shorturl?' + util.toParam({
-            access_token: app.auth.accessToken
+            access_token: authData.accessToken
           });
         request.json(url, data, cb);
       });
--- test/test.js
+++ test/test.js
@@ -9,12 +9,12 @@ describe('node-weixin-link node module', function () {
     secret: process.env.APP_SECRET,
     token: process.env.APP_TOKEN
   };
-  var auth = require("node-weixin-auth");
+
   var config = require("node-weixin-config");
   config.app.init(app);
 
   it('should be able to create a temporary qrcode', function (done) {
-    link.qrcode.temporary.create(app, auth, 10, function (error, json) {
+    link.qrcode.temporary.create(app, 10, function (error, json) {
       assert.equal(true, validator.isURL(json.url));
       assert.equal(true, json.expire_seconds <= 7 * 3600 * 24);
       assert.equal(true, typeof json.ticket === 'string');
@@ -24,7 +24,7 @@ describe('node-weixin-link node module', function () {
 
   it('should be able to create a permanent qrcode', function (done) {
 
-    link.qrcode.permanent.create(app, auth, 10, function (error, json) {
+    link.qrcode.permanent.create(app, 10, function (error, json) {
       assert.equal(true, validator.isURL(json.url));
       assert.equal(true, typeof json.ticket === 'string');
       done();
@@ -32,7 +32,7 @@ describe('node-weixin-link node module', function () {
   });
 
   it('should be able to create a permanent string qrcode', function (done) {
-    link.qrcode.permanent.createString(app, auth, 'heleoodo', function (error, json) {
+    link.qrcode.permanent.createString(app, 'heleoodo', function (error, json) {
       assert.equal(true, validator.isURL(json.url));
       assert.equal(true, typeof json.ticket === 'string');
       done();
@@ -41,7 +41,7 @@ describe('node-weixin-link node module', function () {
 
   it('should be able to shorten a url', function (done) {
     var url = 'http://mp.weixin.qq.com/wiki/3/17e6919a39c1c53555185907acf70093.html';
-    link.url.shorten(app, auth, url, function (error, json) {
+    link.url.shorten(app, url, function (error, json) {
       assert.equal(true, validator.isURL(json.short_url));
       assert.equal(true, json.errcode === 0);
       assert.equal(true, json.errmsg === 'ok');
